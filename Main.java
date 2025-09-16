1a7588d96111ebc24d469545c990bccf28ddabd6
Nhat Tram Nguyen <145398919+tramnngy@users.noreply.github.com>
2025-09-16 21:46:16 +0000

    finish works

--- Main.java
+++ Main.java
@@ -8,25 +8,45 @@ public class Main
         Scanner scan = new Scanner(System.in);
         
         int numOfNums;
+        double number1;
+        double number2;
         System.out.println("How many random numbers do you want to see (must be at least 1)?");
         numOfNums = scan.nextInt();
-        printRandom(numOfNums);
+        printRandom3(numOfNums);
+
+        double slope = calcSlope (0,1,1,0);
+        System.out.println ( "The slope is " + slope);
+
+         System.out.println("enter a double");
+        number1 = scan.nextDouble();
+         System.out.println("enter a double");
+        number2 = scan.nextDouble();
+         System.out.println("distance = " + roundedDist(number1, number2));
     }
     
     public static void printRandom3(int num)
     {
         // Do problem 1 here
+        int lowerBound = 2; 
+        int upperBound = num + 2; 
+        System.out.println( (int) (Math.random () * (upperBound - lowerBound + 1) + lowerBound));
+        System.out.println( (int) (Math.random () * (upperBound - lowerBound + 1) + lowerBound));
+        System.out.println( (int) (Math.random () * (upperBound - lowerBound + 1) + lowerBound));
     }
     
-    public static double calcSlope(double x1, double x2, double y1, double y2)
+    public static double calcSlope(double x1, double y1, double x2, double y2)
     {
         // Do problem 2 here; replace code below
-        return 0.0;
+        
+        double slope = (y2-y1)/(x2-x1);
+
+        return slope;
     }
     
     public static int roundedDist(double a, double b)
     {
-        // Do problem 3 here; replace code below
-        return 0;
+        int rounded = (int)(Math.round(Math.abs(a - b)));
+
+        return rounded;
     }
 }
